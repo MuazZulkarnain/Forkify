dec4680faf3261e2fab0c1027fe0d7cc3dcf004c
MuazZulkarnain <idmuazzulkarnain@gmail.com>
2024-08-06 13:25:20 +0800

    Init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2d68757f-bd53-41bf-b8d0-ae4278224e8b';
+export const MODAL_CLOSE_SEC = 2.5;
